8b706b487b6e0daa88128128a9a858f77b352aee
Sudharsan B <71257512+sudharsanbaskars@users.noreply.github.com>
2022-11-16 20:47:58 +0530

    Update training.py

--- Project Development Phase/Sprint 3/training.py
+++ Project Development Phase/Sprint 3/training.py
@@ -2,16 +2,17 @@ import pandas as pd
 from train_validation_insertion import TrainingFilesValidation as train_validation
 from training_model import TrainModel as trainModel
 
+"""
+Starting point of the training
 def start_training():
     try:
         path = 'Training_BatchFiles/'
-        # df = pd.read_csv(path)
 
-        # train_valObj = train_validation(path)  # object initialization
-        # train_valObj.train_validation()  # calling the training_validation function
+        # Entry point of the data validation and data preprocessing stages
+        train_valObj = train_validation(path)  # object initialization
+        train_valObj.train_validation()  # calling the training_validation function
 
-        trainModelObj = trainModel()  # object initialization
-        trainModelObj.model_training()  # training the model for the files in the table
+
 
     except Exception as e:
         print("Something went wrong in start_training.The exception is "+str(e))
